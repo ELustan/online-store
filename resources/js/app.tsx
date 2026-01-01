b1af87d02bf6e5ecb7cfb8e3d3e76afeb9978a0f
Exequiel Lustan <lustan.tikel20@gmail.com>
2026-01-01 15:25:07 +0800

    include public/build

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -10,7 +10,7 @@ import "@fontsource/poppins"; // Defaults to weight 400
 import "@fontsource/poppins/400.css"; // Specify weight
 import "@fontsource/poppins/400-italic.css"; // Specify weight and style
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'O! Store';
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
